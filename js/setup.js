cf89962a2d19bc51f28788fce839766f28b121b4
tasownzt <tas-93@hotmail.com>
2021-10-23 23:24:21 -0300

    Adding the api and buttons in each items

--- js/setup.js
+++ js/setup.js
@@ -16,3 +16,29 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+let apiFilm = {
+    title: '',
+    description: '',
+    url_cape: '',
+    trailer_code: '',
+    movie_link: ''
+}
+
+async function getData(file) {
+    const response = await fetch(`./js/api/${file}`);
+    const data = await response.json();
+    
+    apiFilm.title = data.highlight.title;
+    apiFilm.description = data.highlight.description;
+    apiFilm.url_cape = data.highlight.url_cape;
+    apiFilm.trailer_code = data.highlight.trailer_code;
+    apiFilm.movie_link = data.highlight.movie_link;
+    console.log(apiFilm);
+
+    return apiFilm;
+}
+
+getData('films.json');
+/* const data = getData();
+console.log(data.highlight.title); */

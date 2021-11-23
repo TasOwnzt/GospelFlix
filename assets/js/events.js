2f34715970f25e1c6dc06bf46341f04ee144d4db
tasownzt <tas-93@hotmail.com>
2021-11-23 00:26:46 -0300

    Adding component model info and styling

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,10 +1,12 @@
 const closeBtn = document.querySelector("#btn-close-player");
 const showPlayerContainer = document.querySelector(".player-container");
 const player = document.querySelector('#player');
+const modelInfo = document.querySelector("#model-info");
 
 function ShowPlayer(evt) {
     showPlayerContainer.style.display = 'flex';
     closeBtn.style.display = 'flex';
+    modelInfo.style.display = "none";
     player.insertAdjacentHTML("afterbegin", "<iframe src='https://www.youtube.com/embed/3f2CYnOgtDU' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope' allowfullscreen></iframe>");
 };
 
@@ -15,4 +17,10 @@ function ClosePlayer(evt) {
     }
     player.innerHTML = '';
     player.appendChild(closeBtn);
+}
+
+function ShowInfo() {
+    showPlayerContainer.style.display = "flex";
+    player.style.display = 'none';
+    modelInfo.style.display = "flex";
 }
